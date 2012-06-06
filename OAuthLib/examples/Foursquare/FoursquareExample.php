52bb463df8dee175677f9c84ba51a835411087e4
Chathuranga Tennakoon <chathuranga.t@gmail.com>
2012-06-06 17:47:55 +0530

    FoursquareProvider and FoursquareExample were added

---
+++ OAuthLib/examples/Foursquare/FoursquareExample.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by
+ * Author : Chathuranga Tennakoon
+ * Email  : chathuranga.t@gmail.com
+ * Blog   : http://chathurangat.blogspot.com
+ * Date   : 6/6/12
+ * Time   : 4:02 PM
+ * IDE    : JetBrains PhpStorm
+ *
+ */
+
+
+echo "Foursquare Example";
+
+?>
---
+++ OAuthLib/providers/classes/FoursquareProvider.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by
+ * Author : Chathuranga Tennakoon
+ * Email  : chathuranga.t@gmail.com
+ * Blog   : http://chathurangat.blogspot.com
+ * Date   : 6/6/12
+ * Time   : 4:13 PM
+ * IDE    : JetBrains PhpStorm
+ *
+ */
+class FoursquareProvider
+{
+
+}
