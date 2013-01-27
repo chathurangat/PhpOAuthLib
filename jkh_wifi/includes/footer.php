8ccf4896bb76f3691b4d7c72e80446db34f1c2c5
darshanac <darshanac@hsenidmobile.com>
2013-01-27 11:47:15 +0530

    Unnecessary filed were removed

--- jkh_wifi/includes/footer.php
+++
@@ -1,3 +0,0 @@
-<small> 
-						&#169; Copyright 2012<?php //echo gmdate("Y");?> Lanka Communication Services (Pvt.) Ltd. | Powered by <a href="http://www.lankacom.net/">Lanka Communication Services (Pvt.) Ltd.</a> | <a href="#">Top</a>
-				</small>
